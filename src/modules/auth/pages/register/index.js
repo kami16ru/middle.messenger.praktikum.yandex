1ee7e0de0d9f1b151299e303e636580c81f1cf10
Kamil Khismatullin <kami16ru@gmail.com>
2022-08-16 19:08:21 +0300

    register page validations

--- src/modules/auth/pages/register/index.js
+++ src/modules/auth/pages/register/index.js
@@ -1,21 +1,71 @@
+import './style.css'
 import template from './template.hbs'
 import Component from '../../../../lib/dom/Component'
 import Button from '../../../../components/ui/button'
 import { loading } from '../../../../lib/helpers/components'
 import Input from '../../../../components/ui/input'
+import Validator from '../../../../lib/validation/Validator'
 
 const registerBtnId = 'register-submit'
 const redirectLoginBtnId = 'register-go-login'
 const registerLoadingId = 'register-submit-loading'
 
 const form = {
-  email: { id: 'form-register-email', name: 'email', label: 'Почта', helper: 'Email пользователя' },
-  login: { id: 'form-register-login', name: 'login', label: 'Логин' },
-  first_name: { id: 'form-register-first-name', name: 'first_name', label: 'Имя', helper: 'Как вас зовут?' },
-  second_name: { id: 'form-register-second-name', name: 'second_name', label: 'Фамилия' },
-  phone: { id: 'form-register-phone', name: 'phone', label: 'Телефон' },
-  password: { id: 'form-register-password', name: 'password', label: 'Пароль', helper: 'Минимум 8 символов' },
-  passwordConfirm: { id: 'form-register-password-confirm', label: 'Пароль еще раз', helper: 'Должны совпадать' },
+  email: { id: 'form-register-email', name: 'email', label: 'Почта', helper: 'Email пользователя', rules: ['isEmail'] },
+  login: { id: 'form-register-login', name: 'login', label: 'Логин', rules: ['isLogin'] },
+  first_name: { id: 'form-register-first-name', name: 'first_name', label: 'Имя', helper: 'Как вас зовут?', rules: ['isName'] },
+  second_name: { id: 'form-register-second-name', name: 'second_name', label: 'Фамилия', rules: ['isName'] },
+  phone: { id: 'form-register-phone', name: 'phone', label: 'Телефон', helper: 'От 10 до 15 символов, состоит из цифр, может начинается с плюса', rules: ['isPhone'] },
+  password: { id: 'form-register-password', name: 'password', label: 'Пароль', helper: 'Хотя бы одна заглавная буква, цифра, минимум 8 символов, максимум 40', rules: ['isPassword'] },
+  passwordConfirm: { id: 'form-register-password-confirm', label: 'Пароль еще раз', helper: 'Должны совпадать', rules: ['isPassword'] }
+}
+const inputs = {
+  InputEmail: Input.template({
+    ...Input.props,
+    input: form.email
+  }),
+  InputPassword: Input.template({
+    ...Input.props,
+    input: form.password
+  }),
+  InputLogin: Input.template({
+    ...Input.props,
+    input: form.login
+  }),
+  InputFirstName: Input.template({
+    ...Input.props,
+    input: form.first_name
+  }),
+  InputSecondName: Input.template({
+    ...Input.props,
+    input: form.second_name
+  }),
+  InputPhone: Input.template({
+    ...Input.props,
+    input: form.phone
+  }),
+  InputPasswordConfirm: Input.template({
+    ...Input.props,
+    input: form.passwordConfirm
+  })
+}
+
+const buttons = {
+  RedirectLoginBtn: Button.template({
+    ...Button.props,
+    class: 'white',
+    value: 'Уже зарегистрированы',
+    href: '/login',
+    id: redirectLoginBtnId,
+    outline: true
+  }),
+  RegisterBtn: Button.template({
+    ...Button.props,
+    class: 'bg-primary white',
+    value: 'Создать аккаунт',
+    href: '/',
+    id: registerBtnId
+  })
 }
 
 class RegisterPage extends Component {
@@ -25,6 +75,7 @@ class RegisterPage extends Component {
 
   async mounted() {
     super.mounted()
+    this.initValidation()
 
     const submitBtn = document.getElementById(registerBtnId)
 
@@ -34,45 +85,63 @@ class RegisterPage extends Component {
 
       if (!anchor) return
 
-      await submitRegisterForm()
+      await this.submitRegisterForm(submitBtn)
 
       window.location = anchor.getAttribute('href')
     })
+  }
 
-    function submitRegisterForm() {
-      const loadingElement = document.getElementById(registerLoadingId)
-      const form = document.getElementById('register-form')
+  initValidation() {
+    const validator = new Validator({ form })
+    const formElements = Object.keys(form).reduce((acc, cur) => {
+      return Object.assign(acc, { [cur]: document.getElementById(form[cur].id) })
+    }, {})
 
-      const formData = new FormData(form)
-      const email = formData.get('email')
-      const login = formData.get('login')
-      const first_name = formData.get('first_name')
-      const second_name = formData.get('second_name')
-      const phone = formData.get('phone')
-      const password = formData.get('password')
-
-      const model = {
-        email,
-        login,
-        first_name,
-        second_name,
-        phone,
-        password
-      }
-
-      loading({ target: submitBtn, loadingElement, loading: true })
-
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          form.reset()
-
-          loading({ target: submitBtn, loadingElement, loading: false })
-          console.log(model)
-
-          resolve()
-        }, 2000)
+    Object.entries(formElements).forEach(([field, element]) => {
+      element.onblur = (e) => validator.onBlurCallback({
+        target: element,
+        messageContainer: element.closest('label').querySelector('.input-helper'),
+        defaultValue: form[field].helper,
+        fieldRules: form[field].rules
       })
+
+      element.onfocus = element.onblur
+    })
+  }
+
+  submitRegisterForm(submitBtn) {
+    const loadingElement = document.getElementById(registerLoadingId)
+    const form = document.getElementById('register-form')
+
+    const formData = new FormData(form)
+    const email = formData.get('email')
+    const login = formData.get('login')
+    const first_name = formData.get('first_name')
+    const second_name = formData.get('second_name')
+    const phone = formData.get('phone')
+    const password = formData.get('password')
+
+    const model = {
+      email,
+      login,
+      first_name,
+      second_name,
+      phone,
+      password
     }
+
+    loading({ target: submitBtn, loadingElement, loading: true })
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        form.reset()
+
+        loading({ target: submitBtn, loadingElement, loading: false })
+        console.log(model)
+
+        resolve()
+      }, 2000)
+    })
   }
 }
 
@@ -80,51 +149,9 @@ export default new RegisterPage({
   template,
   props: {
     form,
-    registerBtnId,
     loadingId: registerLoadingId,
-    RedirectLoginBtn: Button.template({
-      ...Button.props,
-      class: 'white',
-      value: 'Уже зарегистрированы',
-      href: '/login',
-      id: redirectLoginBtnId,
-      outline: true
-    }),
-    RegisterBtn: Button.template({
-      ...Button.props,
-      class: 'bg-primary white',
-      value: 'Создать аккаунт',
-      href: '/',
-      id: registerBtnId
-    }),
-    InputEmail: Input.template({
-      ...Input.props,
-      input: form.email
-    }),
-    InputPassword: Input.template({
-      ...Input.props,
-      input: form.password
-    }),
-    InputLogin: Input.template({
-      ...Input.props,
-      input: form.login
-    }),
-    InputFirstName: Input.template({
-      ...Input.props,
-      input: form.first_name
-    }),
-    InputSecondName: Input.template({
-      ...Input.props,
-      input: form.second_name
-    }),
-    InputPhone: Input.template({
-      ...Input.props,
-      input: form.phone
-    }),
-    InputPasswordConfirm: Input.template({
-      ...Input.props,
-      input: form.passwordConfirm
-    })
+    ...buttons,
+    ...inputs
   },
   components: { Input }
 })
